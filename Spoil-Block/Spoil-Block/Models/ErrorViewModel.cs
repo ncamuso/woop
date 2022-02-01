82bba5bccff40e34d9142f4896425dcce340ac55
Ankit Shrestha <ashrestha18@mail.wou.edu>
2022-02-01 14:30:25 -0800

    Initial project created

---
+++ Spoil-Block/Spoil-Block/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Spoil_Block.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
