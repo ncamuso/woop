1376f2d2e4992efe34bfa5f1ba21d1e655389095
Ankit Shrestha <ashrestha18@mail.wou.edu>
2022-02-01 14:54:30 -0800

    Default boilerplates removed

--- Spoil-Block/Spoil-Block/Controllers/HomeController.cs
+++ Spoil-Block/Spoil-Block/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Spoil_Block.Models;
+
 
 namespace Spoil_Block.Controllers
 {
@@ -23,15 +23,7 @@ namespace Spoil_Block.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
+        
     }
 }
