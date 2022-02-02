f84e519a92d65236272a8bd953e59954f1d82bba
ncamuso <ncamuso@my.chemeketa.edu>
2022-02-01 17:30:03 -0800

    WP-8 complete.  Database scaffolded and up and down scripts work.

---
+++ Spoil-Block/Spoil-Block/Models/UserMedium.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Spoil_Block.Models
+{
+    public partial class UserMedium
+    {
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+        public int BlockageLevel { get; set; }
+        [Column("UserID")]
+        public int UserId { get; set; }
+        [Column("MediaID")]
+        public int MediaId { get; set; }
+
+        [ForeignKey(nameof(MediaId))]
+        [InverseProperty(nameof(Medium.UserMedia))]
+        public virtual Medium Media { get; set; }
+        [ForeignKey(nameof(UserId))]
+        [InverseProperty("UserMedia")]
+        public virtual User User { get; set; }
+    }
+}
