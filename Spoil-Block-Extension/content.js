1322a43e9974114ebc2bf6e06896681e494aa0ee
eborello <eborello19@wou.edu>
2022-02-17 20:17:32 -0800

    page load problem fixed. YT video titles now readable

--- Spoil-Block-Extension/content.js
+++ Spoil-Block-Extension/content.js
@@ -1,1 +1,33 @@
-alert('Welcome to Spoil Block')
+//alert('Welcome to Spoil Block')
+
+//const myElement = document.getElementById("video-title");
+//    myElement.innerText = "spoiler";
+
+async function IsLoaded() {
+    let el = document.getElementById("video-title");
+    if (el) {
+        console.log(el);
+        el.innerHTML += "spoiler";
+    } else {
+        console.log("Not yet loaded");
+    }
+}
+
+function pageWait() {
+return new Promise(resolve => {
+    setTimeout(() => {
+    IsLoaded();
+    }, 1000);
+});
+}
+  
+async function asyncCall() {
+console.log('calling');
+const result = await pageWait();
+
+console.log(result);
+
+// expected output: "resolved"
+}
+
+asyncCall();
