e73b41c93b055d4d5e19b016b7a5206356c4190d
eborello <eborello19@wou.edu>
2022-02-22 10:48:38 -0800

    getting all initial youtube video titles on page load

--- Spoil-Block-Extension/content.js
+++ Spoil-Block-Extension/content.js
@@ -3,13 +3,20 @@
 //const myElement = document.getElementById("video-title");
 //    myElement.innerText = "spoiler";
 
+function blurElement (element) {
+    element.style.color = 'transparent';
+    element.style.textShadow = '0 0 5px rgba(0,0,0,0.5)';
+}
+
 async function IsLoaded() {
     let el = document.getElementById("video-title");
+    
     if (el) {
         console.log(el);
         //el.innerHTML += "spoiler";
-        el.style.color = 'transparent';
-        el.style.textShadow = '0 0 5px rgba(0,0,0,0.5)';
+        blurElement(el);
+        let els = document.querySelectorAll('[id=video-title]');
+        console.log(els);
     } else {
         console.log("Not yet loaded");
     }
@@ -19,17 +26,19 @@ function pageWait() {
 return new Promise(resolve => {
     setTimeout(() => {
     IsLoaded();
-    }, 100);
+    }, 200);
 });
 }
   
 async function asyncCall() {
-console.log('calling');
-const result = await pageWait();
+    console.log('calling');
+    const result = await pageWait();
 
-console.log(result);
+    console.log(result);
 
 // expected output: "resolved"
 }
 
 asyncCall();
+
+// DOM Subtree Modified
