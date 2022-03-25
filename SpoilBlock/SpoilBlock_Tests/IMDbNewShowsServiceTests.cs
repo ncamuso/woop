e38fe1a4ae06b76096999ab4f16846d86a801760
Ankit Shrestha <ashrestha18@mail.wou.edu>
2022-03-25 14:24:23 -0700

    Added tests

---
+++ SpoilBlock/SpoilBlock_Tests/IMDbNewShowsServiceTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
+using SpoilBlock.DAL.Abstract;
+using SpoilBlock.DAL.Concrete;
+using SpoilBlock.Models;
+using System.Net.Http;
+using Moq.Contrib.HttpClient;
+
+
+namespace SpoilBlock_Tests
+{
+    public class IMDbNewShowsServiceTests
+    {
+        private NewShowsServiceData _newShowsServiceData = new NewShowsServiceData();
+
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test_NewShowsService_SearchCorrectlyInterpretsJSON()
+        {
+            IMDbNewShowsService service = new IMDbNewShowsService(new Mock<IHttpClientFactory>().Object, new APIKeyAccessor(""));
+
+            var result = service.ParseIMDbUpComingJSON(_newShowsServiceData.validIMDbJSON).Item1.ToList();
+            List<IMDbUpComing> expected = _newShowsServiceData.expectedValidIMDbJSONResponse.items.ToList();
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Test_NewShowsService_ShouldReturnEmptyList()
+        {
+            IMDbNewShowsService service = new IMDbNewShowsService(new Mock<IHttpClientFactory>().Object, new APIKeyAccessor(""));
+
+            var result = service.ParseIMDbUpComingJSON(_newShowsServiceData.validIMDbJSON).Item1.ToList();
+            List<IMDbUpComing> expected = new List<IMDbUpComing>();
+            Assert.Equals(result.Count, 0);
+        }
+
+        [Test]
+        public void Test_SearchService_Throws_ArgumentNullException_If_JSON_is_null()
+        {
+            IMDbNewShowsService service = new IMDbNewShowsService(new Mock<IHttpClientFactory>().Object, new APIKeyAccessor(""));
+
+            Assert.Throws<ArgumentNullException>(delegate { service.ParseIMDbUpComingJSON(null); });
+        }
+
+    }
+}
