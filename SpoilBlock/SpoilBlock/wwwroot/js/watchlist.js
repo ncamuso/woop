35e8a7d3fca864f3f297bf802d5839d0fd57c910
Ankit Shrestha <ashrestha18@mail.wou.edu>
2022-04-05 07:57:23 -0700

    Dynamic sorting working but not enough item in the list

--- SpoilBlock/SpoilBlock/wwwroot/js/watchlist.js
+++ SpoilBlock/SpoilBlock/wwwroot/js/watchlist.js
@@ -1,13 +1,65 @@
-﻿(function () {
-    'use strict';
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const table = document.getElementById('sortTable');
+    const headers = table.querySelectorAll('th');
+    const tableBody = table.querySelector('tbody');
+    const rows = tableBody.querySelectorAll('tr');
 
-    angular.module('watchlist', [
-        // Angular modules 
-        'ngRoute'
+    
+    const directions = Array.from(headers).map(function (header) {
+        return '';
+    });
 
-        // Custom modules 
+    
+    const transform = function (index, content) {
+        
+        headers[index].getAttribute('data-type');
+        return content;
+        
+    };
+    const sortTitle = function (index) {
+        
+        const direction = directions[index] || 'asc';
 
-        // 3rd Party Modules
+        
+        const multiplier = direction === 'asc' ? 1 : -1;
 
-    ]);
-})();
+        const newRows = Array.from(rows);
+
+        newRows.sort(function (rowA, rowB) {
+            const cellA = rowA.querySelectorAll('td')[index].innerHTML;
+            const cellB = rowB.querySelectorAll('td')[index].innerHTML;
+
+            const a = transform(index, cellA);
+            const b = transform(index, cellB);
+
+            switch (true) {
+                case a > b:
+                    return 1 * multiplier;
+                case a < b:
+                    return -1 * multiplier;
+                case a === b:
+                    return 0;
+            }
+        });
+
+        // Remove old rows
+        [].forEach.call(rows, function (row) {
+            tableBody.removeChild(row);
+        });
+
+        // Reverse the direction
+        directions[index] = direction === 'asc' ? 'desc' : 'asc';
+
+        // Append new row
+        newRows.forEach(function (newRow) {
+            tableBody.appendChild(newRow);
+        });
+    };
+
+    [].forEach.call(headers, function (header, index) {
+        header.addEventListener('click', function () {
+            sortTitle(index);
+        });
+    });
+
+});
