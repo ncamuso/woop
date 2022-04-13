f591a7a8114f0c5969d117c5669831c3b0f49e35
eborello <eborello19@wou.edu>
2022-04-13 10:37:18 -0700

    targeted video-title node instead of innerHTML

--- Spoil-Block-Extension/youtube-block-improved.js
+++ Spoil-Block-Extension/youtube-block-improved.js
@@ -4,7 +4,7 @@ function getContents() {
     console.log('calling');
     try {
         for (let i = 0; i < 9; i++) {
-            videoTitles.push(document.querySelectorAll('#video-title')[i].innerHTML);
+            videoTitles.push(document.querySelectorAll('#video-title')[i]);
         }
     } catch (error) {
         console.log(error)
@@ -19,12 +19,16 @@ function getContents() {
         }, 100);
     } else {
         console.log(div);
-        document.getElementById('contents').addEventListener("DOMNodeInserted", function (event) {
-            if (event.path.some(e => e.id === 'video-title')) {
-                if (event.path[0].nodeName === '#text') {
-                    console.log(event.path);
-                    var newItem = event.path[0].nodeValue;
+        document.getElementById('contents').addEventListener("DOMNodeInserted", function (node) {
+            if (node.path.some(e => e.id === 'video-title')) {
+                if (node.path[0].nodeName === '#text') {
+                    console.log(node.path);
+                    //var newItem = node.path[0].nodeValue;
+                    var newItem = node.path[1];
                     videoTitles.indexOf(newItem) === -1 ? videoTitles.push(newItem) : console.log();
+                    /*
+
+                    */
                     console.log("videos on screen: " + document.querySelectorAll('#video-title').length);
                     console.log("ARRAY OF VIDEO TITLES: "+videoTitles);
                     console.log("LENGTH OF VIDEO TITLES ARRAY: " + videoTitles.length);
