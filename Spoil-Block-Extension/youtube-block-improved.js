3398c55edade64d7e9093b6f740b4fa457f5c06a
eborello <eborello19@wou.edu>
2022-04-13 13:00:57 -0700

    blurring spoilers on home page

--- Spoil-Block-Extension/youtube-block-improved.js
+++ Spoil-Block-Extension/youtube-block-improved.js
@@ -1,4 +1,33 @@
 let videoTitles = [];
+let spoilersOnPage = 0;
+
+function blurTitle (element) {
+    element.style.color = 'transparent';
+    element.style.textShadow = '0 0 5px rgba(0,0,0,0.5)';
+}
+
+function blurThumbnail (element) {
+    element.style.filter = 'blur(10px)';
+}
+
+function checkSpoiler(title) {
+    //let title = element.querySelector("#video-title");
+    //let title = element.querySelector('#video-title');
+    //console.log(title.closest(".style-scope ytd-rich-grid-media").querySelector('#img'));
+    var thumbnail = title.closest(".style-scope ytd-rich-grid-media").querySelector('#img');
+
+    let titleText = title.innerHTML.toString().toLowerCase();
+    let isSpoiler = false;
+
+    if (titleText.includes('spoiler') && !titleText.includes('no spoiler')) { isSpoiler = true; }
+    if (titleText.includes('spoiler-free') || titleText.includes('spoiler free') || titleText.includes('non-spoiler')) {isSpoiler = false; }
+
+    if(isSpoiler) {
+        blurTitle(title);
+        blurThumbnail(thumbnail);
+    }
+    return isSpoiler;
+}
 
 function getContents() {
     console.log('calling');
@@ -25,13 +54,20 @@ function getContents() {
                     console.log(node.path);
                     //var newItem = node.path[0].nodeValue;
                     var newItem = node.path[1];
-                    videoTitles.indexOf(newItem) === -1 ? videoTitles.push(newItem) : console.log();
+                    if (videoTitles.indexOf(newItem) === -1) {
+                        videoTitles.push(newItem);
+                        if (checkSpoiler(newItem)) {
+                            spoilersOnPage++;
+                        }
+                    }
+                    //videoTitles.indexOf(newItem) === -1 ? videoTitles.push(newItem) : console.log();
                     /*
 
                     */
                     console.log("videos on screen: " + document.querySelectorAll('#video-title').length);
                     console.log("ARRAY OF VIDEO TITLES: "+videoTitles);
                     console.log("LENGTH OF VIDEO TITLES ARRAY: " + videoTitles.length);
+                    console.log("Spoilers on page: " + spoilersOnPage);
                 }
             }
         });
