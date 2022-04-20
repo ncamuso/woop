f4e03240de2b89c5a3c50597ed4850c8450d5647
eborello <eborello19@wou.edu>
2022-04-20 15:44:04 -0700

    ready for merge

--- Spoil-Block-Extension/watchlist-update.js
+++ Spoil-Block-Extension/watchlist-update.js
@@ -1,1 +1,13 @@
-console.log("hello from watchlist-update");
+// console.log("hello from watchlist-update");
+
+// var titles = [];
+
+// try {
+//     document.querySelectorAll('.imageandname').forEach( element => {
+//         titles.push(element.textContent.trim());
+//     });
+// } catch (error) {
+//     console.log(error);
+// }
+
+// console.log(titles);
