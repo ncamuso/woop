263c140e5c5111c488b651fa3d15c8afc092ed0f
eborello <eborello19@wou.edu>
2022-04-20 15:51:23 -0700

    reading titles of media on watchlist

--- Spoil-Block-Extension/watchlist-update.js
+++ Spoil-Block-Extension/watchlist-update.js
@@ -1,13 +1,13 @@
-// console.log("hello from watchlist-update");
+console.log("hello from watchlist-update");
 
-// var titles = [];
+var titles = [];
 
-// try {
-//     document.querySelectorAll('.imageandname').forEach( element => {
-//         titles.push(element.textContent.trim());
-//     });
-// } catch (error) {
-//     console.log(error);
-// }
+try {
+    document.querySelectorAll('.imageandname').forEach( element => {
+        titles.push(element.textContent.trim());
+    });
+} catch (error) {
+    console.log(error);
+}
 
-// console.log(titles);
+console.log(titles);
