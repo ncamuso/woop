6a0debdaf76f740cd4f021eb3abebcc1919a673c
Ankit Shrestha <ashrestha18@mail.wou.edu>
2022-04-25 15:57:24 -0700

    changed to the watchlist.css

--- SpoilBlock/SpoilBlock/Program.cs
+++ SpoilBlock/SpoilBlock/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddScoped<IWoopUserMediumRepository, WoopUserMediumRepository>(
 builder.Services.AddScoped<IWoopuserRepository, WoopuserRepository>();
 
 
-builder.Services.AddSingleton<IAPIKeyAccessor>(k => new APIKeyAccessor(builder.Configuration["IMDbServiceApiKey"]));
+builder.Services.AddSingleton<IAPIKeyAccessor>(k => new APIKeyAccessor(builder.Configuration["IMDbServiceApiKe"]));
 
 
 //var connectionString = builder.Configuration.GetConnectionString("WOOPServerConnection"); builder.Services.AddDbContext<WOOPDbContext>(options =>
