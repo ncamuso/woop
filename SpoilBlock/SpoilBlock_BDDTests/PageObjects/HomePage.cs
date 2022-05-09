a09a81c1ba26a6ff63dd17c6db1856f0cbe7dbf3
Ankit Shrestha <ashrestha18@mail.wou.edu>
2022-05-09 13:15:35 -0700

    Added the specflow files

---
+++ SpoilBlock/SpoilBlock_BDDTests/PageObjects/HomePage.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium;
+using SpecFlow.Actions.Selenium;
+using System.Collections.ObjectModel;
+
+
+namespace SpoilBlock_BDDTests.PageObjects
+{
+    public class HomePage : Page
+    {
+        //need to change elements to our homepage html elements& id
+
+        private IWebElement Title => _browserInteractions.WaitAndReturnElement(By.Id("title"));
+        private IWebElement WelcomeText => _browserInteractions.WaitAndReturnElement(By.Id("loggedin-welcome"));
+        //private IEnumerable<IWebElement> AppleButtons => _browserInteractions.WaitAndReturnElements(By.CssSelector("#listOfApples button"));
+
+        public HomePage(IBrowserInteractions browserInteractions)
+            : base(browserInteractions)
+        {
+            PageName = Common.HomePageName;
+        }
+
+        public string GetTitle => Title.Text;
+        public string GetWelcomeText => WelcomeText.Text;
+
+        public string GetAppleButtonText(int index) => AppleButtons.ElementAt(index).Text;
+
+        public IEnumerable<string> GetAppleButtonTexts() => AppleButtons.Select(x => x.Text);
+
+        public void ClickAppleButton(int index)
+        {
+            AppleButtons.ElementAt(index).Click();
+        }
+
+        public void SaveAllCookies()
+        {
+            ReadOnlyCollection<Cookie> cookies = _browserInteractions.GetCookies();
+            FileUtils.SerializeCookiesToFile(Common.CookieFile, cookies);
+        }
+
+        public void LoadAllCookies()
+        {
+            List<Cookie> cookies = FileUtils.DeserializeCookiesFromFile(Common.CookieFile);
+            foreach (Cookie cookie in cookies)
+            {
+                _browserInteractions.AddCookie(cookie);
+            }
+            _browserInteractions.RefreshPage();
+        }
+
+        public void DeleteCookies()
+        {
+            _browserInteractions.DeleteAllCookies();
+        }
+
+    }
+}
---
+++ SpoilBlock/SpoilBlock_BDDTests/StepDefinitions/UserLoginStepDefinitions.cs
@@ -0,0 +1,48 @@
+﻿using TechTalk.SpecFlow;
+using SpoilBlock_BDDTests.PageObjects;
+
+namespace SpoilBlock_BDDTests.StepDefinitions
+{
+
+    public class TestUser
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+    }
+
+    [Binding]
+    public sealed class UserLoginStepDefinitions
+    {
+        private readonly ScenarioContext _scenarioContext;
+        private readonly LoginPage _loginPage;
+        private readonly HomePage _homePage;
+
+
+        [BeforeScenario("@tag1")]
+        public void BeforeScenarioWithTag()
+        {
+            // Example of filtering hooks using tags. (in this case, this 'before scenario' hook will execute if the feature/scenario contains the tag '@tag1')
+            // See https://docs.specflow.org/projects/specflow/en/latest/Bindings/Hooks.html?highlight=hooks#tag-scoping
+
+            //TODO: implement logic that has to run before executing each scenario
+        }
+
+        [BeforeScenario(Order = 1)]
+        public void FirstBeforeScenario()
+        {
+            // Example of ordering the execution of hooks
+            // See https://docs.specflow.org/projects/specflow/en/latest/Bindings/Hooks.html?highlight=order#hook-execution-order
+
+            //TODO: implement logic that has to run before executing each scenario
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            //TODO: implement logic that has to run after executing each scenario
+        }
+    }
+}
